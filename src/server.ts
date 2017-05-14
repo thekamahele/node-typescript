4a4440b960586ad0124ba2e79fcbea5c3a4aa847
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2017-05-13 17:18:38 -0700

    Set up server

---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import { Express, Request, Response } from 'express';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
+const app: Express = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', (req: Request, res: Response, next) => {
+    res.render('Hello World');
+});
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use((err: any, req: Request, res: Response, next) => {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err: any, req: Request, res: Response, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
